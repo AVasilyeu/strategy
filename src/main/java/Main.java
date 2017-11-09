f21a862ddb5775aa0f2b3cf45c68d25814643b3e
Gilza <gilz@bk.ru>
2017-11-09 17:08:53 +0300

    add strategy pattern

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Duck duck = new RedHatDuck(new FlyWithWings());
         Duck newDuck = new WoodenDuck(new FlyNoWay());
-        duck.fly();
         newDuck.fly();
     }
 }
